e0845aa2461286d8fbcb8ac18c3bea993a247442
Luis Suarez <luisdcobain@gmail.com>
2024-11-24 22:52:48 -0300

    initialize Vue project with Vite, add basic structure and components

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5tY9djnlXdBxcKPaz6paye27Ver-q_2A",
+  authDomain: "desafio-evaluado-49fc2.firebaseapp.com",
+  projectId: "desafio-evaluado-49fc2",
+  storageBucket: "desafio-evaluado-49fc2.firebasestorage.app",
+  messagingSenderId: "682467804690",
+  appId: "1:682467804690:web:737471ac50b949f0987acb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
